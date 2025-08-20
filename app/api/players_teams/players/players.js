1ca15782c9d18eedbd6d5716acbd4d6158a5e5cc
PEDRAM BABAZADEH <68613205+pedrambabazadeh@users.noreply.github.com>
2025-08-20 18:48:07 +0200

    teams and players api initialized

---
+++ app/api/players_teams/players/players.js
@@ -0,0 +1,42 @@
+const players = [
+  {
+    id: 101,
+    name: "Erling Haaland",
+    image: "https://upload.wikimedia.org/wikipedia/commons/1/10/Erling_Haaland_2023.jpg",
+    teamId: 1,
+    seasonGoals: 7,
+    seasonAssists: 2,
+    previousTeams: ["Molde", "RB Salzburg", "Borussia Dortmund"],
+    nationalGoals: 30, // as of 2025 (approx)
+    careerGoals: 200,  // mocked realistic
+    age: 25,
+    height: "1.95 m"
+  },
+  {
+    id: 102,
+    name: "Kylian Mbappé",
+    image: "https://upload.wikimedia.org/wikipedia/commons/4/45/Kylian_Mbapp%C3%A9.jpg",
+    teamId: 2,
+    seasonGoals: 6,
+    seasonAssists: 3,
+    previousTeams: ["AS Monaco", "Paris Saint-Germain"],
+    nationalGoals: 50, // approx by 2025
+    careerGoals: 280, // mocked realistic
+    age: 26,
+    height: "1.78 m"
+  },
+  {
+    id: 103,
+    name: "Lionel Messi",
+    image: "https://upload.wikimedia.org/wikipedia/commons/5/58/Lionel_Messi_2017.jpg",
+    teamId: 3,
+    seasonGoals: 5,
+    seasonAssists: 4,
+    previousTeams: ["FC Barcelona", "Paris Saint-Germain"],
+    nationalGoals: 106, // Argentina record (actual close to 2025)
+    careerGoals: 820, // approximate
+    age: 38,
+    height: "1.70 m"
+  }
+]
+export default players;
---
+++ app/api/players_teams/teams/teams.js
@@ -0,0 +1,19 @@
+const teams = [
+  {
+    id: 1,
+    name: "Manchester City",
+    logo: "https://upload.wikimedia.org/wikipedia/en/e/eb/Manchester_City_FC_badge.svg"
+  },
+  {
+    id: 2,
+    name: "Paris Saint-Germain",
+    logo: "https://upload.wikimedia.org/wikipedia/en/a/a7/Paris_Saint-Germain_F.C..svg"
+  },
+  {
+    id: 3,
+    name: "Inter Miami",
+    logo: "https://upload.wikimedia.org/wikipedia/en/5/5c/Inter_Miami_CF_logo.svg"
+  }
+];
+
+export default teams;
