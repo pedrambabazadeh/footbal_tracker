2f2aa55b27bc9a93eb3abb3e0fd62f8a0e57b154
PEDRAM BABAZADEH <68613205+pedrambabazadeh@users.noreply.github.com>
2025-08-25 10:25:47 +0200

    ratings arrssy and bestPlayers component initialized

---
+++ app/api/players_teams/best-players/BestPlayers.js
@@ -0,0 +1,13 @@
+import players from "../players/players";
+import teams from "../teams/teams";
+
+//imports need to be changes to await import to mock the data base better
+const bestPlayers = players.filter(player => player.rating === 5).map(player => {
+    const team = teams.find(team => team.id === player.teamId);
+    return {
+        ...player,
+        team: team ? team.name : "Unknown",
+        teamLogo: team ? team.logo : "Unknown"
+    };
+});
+export default bestPlayers;
---
+++ app/api/players_teams/rtings/Ratings.js
@@ -0,0 +1,6 @@
+const ratings = [
+  { playerId: 101, rating: 5, week: "2025-W33" },
+  { playerId: 102, rating: 5, week: "2025-W33" },
+  { playerId: 103, rating: 4, week: "2025-W33" }
+];
+export default ratings;
