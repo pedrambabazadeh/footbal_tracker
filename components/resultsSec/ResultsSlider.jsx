8b533b6d00f42f0b0c27fd07c220e3008919024b
PEDRAM BABAZADEH <68613205+pedrambabazadeh@users.noreply.github.com>
2025-10-16 23:50:35 +0200

    scroll added to results but the bug remains

--- components/resultsSec/ResultsSlider.jsx
+++ components/resultsSec/ResultsSlider.jsx
@@ -7,22 +7,35 @@ import { FaCaretLeft, FaCaretRight} from 'react-icons/fa';
 
 const ResultsSlider = ({ data }) => {
 
-    const [, setScrollPosition] = useState(0);
+    const [scrollPosition, setScrollPosition] = useState(0);
 
     const handleScrollLeft = (e) => {
         //by the next start code here for handling the scroll
+        setScrollPosition(scrollPosition - 50.5 );
     }
+    const handleScrollRight = (e) => {
+        //by the next start code here for handling the scroll
+        setScrollPosition(scrollPosition + 50.5 );
+    }
+    // I should handle that .75% difference in transform. also, has to make the infinite scroll using the same data array twice and %
   return (
-          <div id="slider_results" className=" relative flex overflow-x-hidden gap-6 pb-4 scrollbar-hide snap-x snap-mandatory [&>*]:snap-start">
-        <button className="absolute left-0 top-1/2 bg-[var(--primary-yellow-500)] text-black p-2 rounded-full shadow-lg transform -translate-y-1/2 z-10 hover:bg-yellow-400">
+          <div id="slider_results" className=" relative flex overflow-x-hidden  pb-4 scrollbar-hide">
+        <button onClick={handleScrollLeft} className="absolute left-0 top-1/2 bg-[var(--primary-yellow-500)] text-black p-2 rounded-full shadow-lg transform -translate-y-1/2 z-10 hover:bg-yellow-400">
           <FaCaretLeft />
         </button>
-        {data.map((result, index) => (
-          <Card key={index} className="w-[calc(50%-.75rem)]">
-            <InnerResult result={result} />
-          </Card>
-        ))}
-        <button className="absolute right-0 top-1/2 bg-[var(--primary-yellow-500)] text-black p-2 rounded-full shadow-lg transform -translate-y-1/2 z-10 hover:bg-yellow-400">
+          <div className=" max-w-full relative inset-0 flex gap-6 transition-transform duration-300" style={{ transform: `translateX(calc(${scrollPosition}%))`, transition: 'transform 1s ease-in-out' }}>
+            {data.map((result, index) => (
+                <Card className="w-[calc(50%-.75rem)]" key={index}>
+                  <InnerResult result={result} />
+                </Card>
+            ))}
+          {data.map((result, index) => (
+                <Card className="w-[calc(50%-.75rem)]" key={index * 10}>
+                  <InnerResult result={result} />
+                </Card>
+            ))}
+          </div>
+        <button onClick={handleScrollRight} className="absolute right-0 top-1/2 bg-[var(--primary-yellow-500)] text-black p-2 rounded-full shadow-lg transform -translate-y-1/2 z-10 hover:bg-yellow-400">
           <FaCaretRight />
         </button>
       </div>
