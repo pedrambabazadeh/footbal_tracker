c90726d392c55f3c8f191ed84d6fb2d47d098159
PEDRAM BABAZADEH <68613205+pedrambabazadeh@users.noreply.github.com>
2025-10-17 10:40:39 +0200

    results slider optimized, bug remains

--- components/resultsSec/ResultsSlider.jsx
+++ components/resultsSec/ResultsSlider.jsx
@@ -17,13 +17,12 @@ const ResultsSlider = ({ data }) => {
         //by the next start code here for handling the scroll
         setScrollPosition(scrollPosition + 50.5 );
     }
-    // I should handle that .75% difference in transform. also, has to make the infinite scroll using the same data array twice and %
+    // I should handle that .75rem difference in transform. also, has to make the infinite scroll using the same data array twice and %
   return (
           <div id="slider_results" className=" relative flex overflow-x-hidden  pb-4 scrollbar-hide">
         <button onClick={handleScrollLeft} className="absolute left-0 top-1/2 bg-[var(--primary-yellow-500)] text-black p-2 rounded-full shadow-lg transform -translate-y-1/2 z-10 hover:bg-yellow-400">
           <FaCaretLeft />
         </button>
-          <div className=" max-w-full relative inset-0 flex gap-6 transition-transform duration-300" style={{ transform: `translateX(calc(${scrollPosition}%))`, transition: 'transform 1s ease-in-out' }}>
             {data.map((result, index) => (
                 <Card className="w-[calc(50%-.75rem)]" key={index}>
                   <InnerResult result={result} />
@@ -34,7 +33,6 @@ const ResultsSlider = ({ data }) => {
                   <InnerResult result={result} />
                 </Card>
             ))}
-          </div>
         <button onClick={handleScrollRight} className="absolute right-0 top-1/2 bg-[var(--primary-yellow-500)] text-black p-2 rounded-full shadow-lg transform -translate-y-1/2 z-10 hover:bg-yellow-400">
           <FaCaretRight />
         </button>
