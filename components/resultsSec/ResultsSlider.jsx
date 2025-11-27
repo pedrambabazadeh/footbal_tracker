f7117b586a6c73f61564fbd05c8dccc2c17091ce
PEDRAM BABAZADEH <68613205+pedrambabazadeh@users.noreply.github.com>
2025-11-27 08:52:23 +0100

    slider infinite loop bug solved, still performance issue on scrollRight and multiple scrolls

--- components/resultsSec/ResultsSlider.jsx
+++ components/resultsSec/ResultsSlider.jsx
@@ -6,52 +6,58 @@ import {CardGroup, Card} from '@/components/exports/for-home'
 import {default as InnerResult} from './InnerResult'
 import { FaCaretLeft, FaCaretRight} from 'react-icons/fa';
 import { enm } from '@/utils/customFunctions';
+import './slider.css'
 
 
 const ResultsSlider = ({ data }) => {
-
     const [scrollPosition, setScrollPosition] = useState(0);
-    const [sliderStatus, setSliderStatus] = useState([...data]);
+    const sliderStatusRef = useRef([...data]);
     const scrollDirection = enm('left', 'right', 'none');
     const directionRef = useRef(scrollDirection.none);
+    const [isTransitioning, setIsTransitioning] = useState(false);
 
-    const handleScrollLeft = (e) => {
-        //by the next start code here for handling the scroll
-        setScrollPosition(prev => (prev - 50));
-       directionRef.current = scrollDirection.left;
+    const handleScrollLeft = () => {
+      directionRef.current = scrollDirection.left;
+      setIsTransitioning(true);
+      setScrollPosition(prev => (prev - 50));
+       
     }
-    const handleScrollRight = (e) => {
-        //by the next start code here for handling the scroll;
-        setScrollPosition(prev => (prev + 50));
+    const handleScrollRight = () => {
        directionRef.current = scrollDirection.right;
+        setIsTransitioning(true);
+        setScrollPosition(prev => prev + 50);
     }
 
     const handleTransitionEnd = () => {
-       const dir = directionRef.current;
-    if (dir == scrollDirection.none) return; 
-        setSliderStatus(prev => {
-    const arr = [...prev];
-    if (directionRef.current === scrollDirection.left) {
-      arr.push(arr.shift());
-      console.log('scrolled left' + arr);
-      directionRef.current = scrollDirection.none;
-    } else {
-      arr.unshift(arr.pop());
-      console.log('scrolled right' + arr);
-      directionRef.current = scrollDirection.none;
-    }
-      //setScrollPosition(0);
-    
-    return arr;
-  });}
+  setIsTransitioning(false);
+  const arr = [...sliderStatusRef.current];
+
+  if (directionRef.current === scrollDirection.left) {
+    const item = arr.shift();
+    arr.push(item);
+  }
+  else if (directionRef.current === scrollDirection.right) {
+    const item = arr.pop();
+    arr.unshift(item);
+  }
+
+  sliderStatusRef.current = arr;
+  setScrollPosition(0);
+
+  directionRef.current = scrollDirection.none;
+  };
     // I should handle that .75rem difference in transform. also, has to make the infinite scroll using the same data array twice and %
   return (
     <div className='overflow-x-hidden w-full relative'>
-      <button onClick={handleScrollLeft} className="absolute left-0 top-1/2 bg-[var(--primary-yellow-500)] text-black p-2 rounded-full shadow-lg transform -translate-y-1/2 z-10 hover:bg-yellow-400">
+      <button onClick={handleScrollLeft} 
+      className="absolute left-0 top-1/2 bg-[var(--primary-yellow-500)] text-black p-2 rounded-full shadow-lg transform -translate-y-1/2 z-10 hover:bg-yellow-400">
           <FaCaretLeft />
         </button>
-      <div onTransitionEnd={handleTransitionEnd} style={{ transform: `translateX(${scrollPosition}%)` }} id="slider_results" className="transition-transform duration-300 ease-linear relative flex  pb-4 scrollbar-hide">
-            {sliderStatus.map((result, index) => (
+      <div data-direction={directionRef.current} onTransitionEnd={handleTransitionEnd} 
+        style={{ transform: `translateX(${scrollPosition}%)`,
+                  transition: isTransitioning ? "transform 1s" : "none"}} 
+        className=" relative flex  pb-4 scrollbar-hide">
+            { sliderStatusRef.current.map((result, index) => (
                 <div className='w-6/12 shrink-0 px-2' key={index}>
                     <Card >
                       <InnerResult result={result} />
@@ -59,7 +65,8 @@ const ResultsSlider = ({ data }) => {
                   </div>
             ))}
       </div>
-      <button onClick={handleScrollRight} className="absolute right-0 top-1/2 bg-[var(--primary-yellow-500)] text-black p-2 rounded-full shadow-lg transform -translate-y-1/2 z-10 hover:bg-yellow-400">
+      <button onClick={handleScrollRight} 
+      className="absolute right-0 top-1/2 bg-[var(--primary-yellow-500)] text-black p-2 rounded-full shadow-lg transform -translate-y-1/2 z-10 hover:bg-yellow-400">
           <FaCaretRight />
       </button>
   </div>)
