f023a24cc1f2394c35b88b3f14546629b145ebb1
PEDRAM BABAZADEH <68613205+pedrambabazadeh@users.noreply.github.com>
2025-11-28 14:44:06 +0100

    slider problem solved. (removed son's hover class and duplicated array in shallow copy level)

--- components/cards/Card.jsx
+++ components/cards/Card.jsx
@@ -3,10 +3,12 @@ import './card.css'
 
 const Card = ({children, className}) => {
   return (
-    <div className={` card flex-shrink-0 p-6 rounded-xl shadow-md hover:shadow-lg transition ${className}`}>
+    <div className={`card flex-shrink-0 p-6 rounded-xl shadow-md transition ${className}`}>
       {children}
     </div>
   )
 }
 
 export default Card
+
+//hover:shadow-lg  was also used in classes of card but it broke transition in slider once hoverd
--- components/resultsSec/ResultsSlider.jsx
+++ components/resultsSec/ResultsSlider.jsx
@@ -1,8 +1,8 @@
 "use client"
 
 // resolve the problem to show  the removed item at the end when scrolling left and vice versa
-import React, {useEffect, useRef, useState} from 'react'
-import {CardGroup, Card} from '@/components/exports/for-home'
+import React, {useRef, useState} from 'react'
+import { Card, CardGroup} from '@/components/exports/for-home'
 import {default as InnerResult} from './InnerResult'
 import { FaCaretLeft, FaCaretRight} from 'react-icons/fa';
 import { enm } from '@/utils/customFunctions';
@@ -10,62 +10,69 @@ import './slider.css'
 
 
 const ResultsSlider = ({ data }) => {
-    const [scrollPosition, setScrollPosition] = useState(0);
-    const sliderStatusRef = useRef([...data]);
+    const [scrollPosition, setScrollPosition] = useState(-50);
+    const sliderStatusRef = useRef([...data, ...data]);
     const scrollDirection = enm('left', 'right', 'none');
     const directionRef = useRef(scrollDirection.none);
     const [isTransitioning, setIsTransitioning] = useState(false);
 
     const handleScrollLeft = () => {
       directionRef.current = scrollDirection.left;
+      const arr = [...sliderStatusRef.current];
+      sliderStatusRef.current = arr;
       setIsTransitioning(true);
-      setScrollPosition(prev => (prev - 50));
-       
+      setScrollPosition(-100);
     }
     const handleScrollRight = () => {
        directionRef.current = scrollDirection.right;
+       const arr = [...sliderStatusRef.current];
+       sliderStatusRef.current = arr;
         setIsTransitioning(true);
-        setScrollPosition(prev => prev + 50);
+        setScrollPosition(0);
     }
 
-    const handleTransitionEnd = () => {
-  setIsTransitioning(false);
+  const handleTransitionEnd = () => {
   const arr = [...sliderStatusRef.current];
 
   if (directionRef.current === scrollDirection.left) {
-    const item = arr.shift();
-    arr.push(item);
+    arr.push(arr[0]);
+    arr.shift();
   }
   else if (directionRef.current === scrollDirection.right) {
-    const item = arr.pop();
-    arr.unshift(item);
+    arr.unshift(arr[arr.length - 1]);
+    arr.pop();
   }
 
   sliderStatusRef.current = arr;
-  setScrollPosition(0);
+  setScrollPosition(-50);
+  setIsTransitioning(false);
 
   directionRef.current = scrollDirection.none;
   };
-    // I should handle that .75rem difference in transform. also, has to make the infinite scroll using the same data array twice and %
   return (
-    <div className='overflow-x-hidden w-full relative'>
-      <button onClick={handleScrollLeft} 
+    <div className='overflow-x-hidden w-12/12 relative'>
+   <button 
+      onClick={() => handleScrollLeft()}
+      style={{ visibility: isTransitioning ? 'hidden' : 'visible' }} 
       className="absolute left-0 top-1/2 bg-[var(--primary-yellow-500)] text-black p-2 rounded-full shadow-lg transform -translate-y-1/2 z-10 hover:bg-yellow-400">
           <FaCaretLeft />
         </button>
-      <div data-direction={directionRef.current} onTransitionEnd={handleTransitionEnd} 
+      <div onTransitionEnd={handleTransitionEnd} 
         style={{ transform: `translateX(${scrollPosition}%)`,
-                  transition: isTransitioning ? "transform 1s" : "none"}} 
-        className=" relative flex  pb-4 scrollbar-hide">
-            { sliderStatusRef.current.map((result, index) => (
-                <div className='w-6/12 shrink-0 px-2' key={index}>
+                 transition: isTransitioning ? "transform 1s" : "none"
+                 }} 
+        className=" relative flex  pb-4 w-12/12">
+            {sliderStatusRef.current.map((result, index) => (
+                <div className='w-6/12 shrink-0 px-2' key={result.matchId + index}>
                     <Card >
                       <InnerResult result={result} />
                     </Card>
                   </div>
             ))}
       </div>
-      <button onClick={handleScrollRight} 
+      <button 
+      onClick={() => handleScrollRight()} 
+      style={{ visibility: isTransitioning ? 'hidden' : 'visible' }} 
       className="absolute right-0 top-1/2 bg-[var(--primary-yellow-500)] text-black p-2 rounded-full shadow-lg transform -translate-y-1/2 z-10 hover:bg-yellow-400">
           <FaCaretRight />
       </button>
