be882d6692799ddc9fc3672730dd2e4620e482b8
PEDRAM BABAZADEH <68613205+pedrambabazadeh@users.noreply.github.com>
2025-12-08 00:20:41 +0100

    sign-in page improved

--- app/log-in/page.jsx
+++ app/log-in/page.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const SignIn = () => {
   return (
-    <div>here will be sign in page</div>
+    <div className='grid grid-cols-1 lg:grid-cols-2 gap-8 items-center'>
+      
+    </div>
   )
 }
 
